2b178608a36277194f2149d4f8aff95d7369d2ff
m-ji97 <tv5035@naver.com>
2024-03-19 23:27:55 +0900

    240318 수업

--- src/main/java/com/javaex/controller/AttachController.java
+++ src/main/java/com/javaex/controller/AttachController.java
@@ -1,17 +1,35 @@
 package com.javaex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.javaex.service.AttachService;
 
 @Controller
 public class AttachController {
 	
-	@RequestMapping(value="/attach/uploadform")
+	@Autowired
+	private AttachService attachService;
+	
+	@RequestMapping(value="/attach/uploadform", method= {RequestMethod.POST,RequestMethod.GET})
 	public String uploadform() {
 		System.out.println("AttachController.uploadform()");
 		
 		return "attach/form";
 	}
-	//월요일 영상 보고 채워넣기
+	
+	@RequestMapping(value = "/attach/upload", method = RequestMethod.POST)
+	public String upload(@RequestParam(value="file") MultipartFile file) {
+		System.out.println("AttachController.upload()");
+		
+		attachService.exeupload(file);
+		
+		return "";
+	}
+	
 	
 }
